36f324cdd671a53e3081ad60417e2006a1734d56
ManithMam <100299120+ManithMam@users.noreply.github.com>
2023-05-02 14:40:18 +0200

    Implement insertOne test for service

--- backend/src/account/service/accounts.service.spec.ts
+++ backend/src/account/service/accounts.service.spec.ts
@@ -1,18 +1,36 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { AccountsService } from "../service/accounts.service";
-import { getModelToken } from "@nestjs/mongoose";
+import { Schema, getModelToken } from "@nestjs/mongoose";
 import { Account, AccountDocument } from "../schema/account.schema";
-import { Model } from "mongoose";
+import mongoose, { Model, ObjectId } from "mongoose";
+import { CreateAccountDTO } from "../dto/createAccountDTO";
+
+
+const accountMockModel = {     
+    username: 'Max',
+    password: 'password123',
+    games: []
+}
+
+const mockCreateAccountDTO: CreateAccountDTO = {
+    username: 'Max',
+    password: 'savePassword333'
+}
+
 
 describe('AccountsService', () => {
     let service: AccountsService;
-    let model: Model<AccountDocument> 
+    let model: Model<AccountDocument>    
+    
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             providers: [ AccountsService, {
-                provide: getModelToken(Account.name),
-                useValue: { username: 'test', hashedPassword: 'password'}
+                provide: getModelToken('Account'),
+                useValue: { Account, 
+                    create: jest.fn().mockResolvedValue(mockCreateAccountDTO), 
+                    findById: jest.fn().mockResolvedValue(accountMockModel),
+                    save: jest.fn()}
             }]
         }).compile();
 
@@ -24,10 +42,17 @@ describe('AccountsService', () => {
         expect(service).toBeDefined();
     })
 
-    describe('insertOne', () => {
-        it('should create new account', async () => {
-            const newAccount = await service.insertOne({username: 'A User', password: 'password123'})
-            expect(newAccount._id).toBeDefined()
-        })        
-    })
+     describe('insertOne', () => {
+        it('should create new account', async () => {            
+
+            const newAccountResult: { 
+                username: string
+                password: string
+            } = await service.insertOne(mockCreateAccountDTO)
+            
+            expect(newAccountResult).toBeDefined()
+            expect(newAccountResult.username).toBe('Max')   
+            expect(newAccountResult.password).toBe('savePassword333')   
+        })                  
+    })    
 })
