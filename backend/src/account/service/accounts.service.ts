8b1ecc70078a2a35c35feb5718788978d5664270
ManithMam <100299120+ManithMam@users.noreply.github.com>
2023-05-02 14:39:51 +0200

    Add deleteAccount method in service

--- backend/src/account/service/accounts.service.ts
+++ backend/src/account/service/accounts.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, Schema } from 'mongoose';
 import { Account } from '../schema/account.schema';
@@ -14,20 +14,31 @@ export class AccountsService {
          return account
     }
 
-    async insertOne(account: CreateAccountDTO): Promise<Account>{
+    async insertOne(account: CreateAccountDTO): Promise<Account>{        
         const newAccount = await this.accountModel.create(account)        
-        return newAccount       
+        return {
+            username: newAccount.username,
+            password: newAccount.password            
+        }   
+    }
+
+    async deleteAccount(accountId: string) {
+        const accountToDelete = await this.accountModel.findOneAndDelete()
+        if(!accountToDelete){
+            throw new NotFoundException('Game with ' + accountId + ' could not be found.') 
+        }
+        return 'Account with id ' + accountId + ' was deleted.'
     }
 
     async updateUserGamesAdd(gameDto: updateAccountGameDto, id: string):  Promise<Account>{
-        const gameOid = new Schema.Types.ObjectId(gameDto.game)
-        const account = await this.accountModel.findById(id)
-        account.games.push(gameOid)
+        const gameOid = new Schema.Types.ObjectId(gameDto.gameId)
+        const account = await this.accountModel.findById(id)        
+        account.games.push(gameOid)        
         return account.save()
     }
 
     async updateUserGamesDelete(gameDto: updateAccountGameDto, id: string):  Promise<Account>{
-        const gameOid = new Schema.Types.ObjectId(gameDto.game)
+        const gameOid = new Schema.Types.ObjectId(gameDto.gameId)
         const account = await this.accountModel.findById(id)
         const index = account.games.findIndex((_game) => _game === gameOid)
         account.games.splice(index, 1)
