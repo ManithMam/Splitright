787c9d1f3346b9ff00e734927082fddffe7ce11e
Aleksandra Gidionova <a.gidionova@gmail.com>
2023-05-02 08:27:07 +0200

    removed unnecessary code

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,9 +11,7 @@ async function bootstrap() {
   // <--- Swagger
   const config = new DocumentBuilder()
     .setTitle('SplitRight')
-    //.setDescription('The API description')
     .setVersion('1.0')
-    //.addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
