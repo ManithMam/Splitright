a199948243e0f7dbb7a163c79e0cdfe40125ea3c
ManithMam <100299120+ManithMam@users.noreply.github.com>
2023-05-03 22:16:41 +0200

    Add swagger apiProperty() decorator to LobbyDto

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,8 @@ import { LobbyModule } from './lobby/lobby.module';
 import { AccountsModule } from './account/account.module';
 import { AuthModule } from './account/auth/auth.module';
 import { GameModule } from './game/game.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017'), AccountsModule, AuthModule, GameModule, LobbyModule],
