08da094869807aef155bc3091add8ca3036f61bb
ManithMam <100299120+ManithMam@users.noreply.github.com>
2023-05-03 20:25:08 +0200

    Combine createAccountDTO and loginDTO to accountDTO
    Reduce duplicate code

--- backend/src/account/dto/accountDTO.ts
+++ backend/src/account/dto/accountDTO.ts
@@ -1,7 +1,7 @@
 import { IsNotEmpty } from "class-validator"
 import { ApiProperty } from "@nestjs/swagger";
 
-export class CreateAccountDTO {
+export class AccountDto {
     @ApiProperty()
     @IsNotEmpty()
     username: string
