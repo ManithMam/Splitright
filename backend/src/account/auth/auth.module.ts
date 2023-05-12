cfc4aad54d2cb2f7c0cfcad9efec6a32bd97e435
ManithMam <100299120+ManithMam@users.noreply.github.com>
2023-05-12 17:00:36 +0200

    Add LocalStrategy to authModule

--- backend/src/account/auth/auth.module.ts
+++ backend/src/account/auth/auth.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { AccountsModule } from '../account.module';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { LocalStrategy } from './local.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 
 @Module({
-    imports: [AccountsModule],
-    providers: [AuthService],
+    imports: [AccountsModule, PassportModule],
+    providers: [AuthService, LocalStrategy],
     controllers: [AuthController]
 })
 
