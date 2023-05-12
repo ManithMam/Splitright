350e020b292eb63a72a9aaf304f61dad5370c247
ManithMam <100299120+ManithMam@users.noreply.github.com>
2023-05-12 17:01:06 +0200

    Implement login route with guard

--- backend/src/account/auth/auth.controller.ts
+++ backend/src/account/auth/auth.controller.ts
@@ -1,12 +1,14 @@
-import { Body, Controller, Post} from '@nestjs/common';
+import { Body, Controller, Post, UseGuards} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AccountDto } from '../dto/accountDTO';
+import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) {}
 
-    @Post('login')
+    @UseGuards(LocalAuthGuard)
+    @Post('auth/login')
     login(@Body() accountDto: AccountDto){
         return this.authService.login(accountDto)
     }
